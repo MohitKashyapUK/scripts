6ac2e930176c8d0380b7df13885d25cf69de80f1
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2024-05-04 22:21:46 +0530

    Update youtube-video-like-and-dislike-shortcut.js

--- js/youtube-video-like-and-dislike-shortcut.js
+++ js/youtube-video-like-and-dislike-shortcut.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         YouTube video rating shortcuts
+// @name         Give YouTube videos rating by shortcuts
 // @namespace    http://tampermonkey.net/
 // @version      2024-03-17
 // @description  try to take over the world!
-// @author       You
-// @match        https://*.youtube.com/*
+// @author       Mohit
+// @match        *://*.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
@@ -12,11 +12,13 @@
 (function() {
     'use strict';
 
-    window.onload = async () => {
-        const like = () => document.querySelector("#top-level-buttons-computed > segmented-like-dislike-button-view-model > yt-smartimation > div > div > like-button-view-model > toggle-button-view-model > button-view-model > button").click();
-        const dislike = () => document.querySelector("#top-level-buttons-computed > segmented-like-dislike-button-view-model > yt-smartimation > div > div > dislike-button-view-model > toggle-button-view-model > button-view-model > button").click();
+    window.onload = () => {
+        const qs = selector => document.querySelector(selector);
+        const select = "like-button-view-model button";
+        const like = () => qs(select).click();
+        const dislike = () => qs("dis" + select).click();
 
-        window.addEventListener("keydown", (e) => {
+        window.addEventListener("keydown", e => {
             const key = e.key;
             if (key == "+") {
                 like();
