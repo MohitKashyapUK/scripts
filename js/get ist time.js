ec559b3507698323f2bd76596b949c3f5776505d
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2024-12-22 18:00:01 +0530

    Update get ist time.js

--- js/get ist time.js
+++ js/get ist time.js
@@ -1,5 +1,15 @@
 function IST_date() {
-    const date = new Date();
+    const options = {
+        timeZone: 'IST',
+        year: '2-digit',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    };
+    const ist_date = new Date(new Date().toLocaleString('en-US', options));
 
-    return date.toLocaleString('en-IN', { timeZone: 'IST' });
+    return ist_date;
 }
