ea88ea781f3ce0e376a9d8119101d84affb926c5
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2025-09-12 03:06:30 +0530

    Create yt-dlp.sh

---
+++ Shell Script/yt-dlp.sh
@@ -0,0 +1,8 @@
+### '--cookies <file_path>', use cookies if YT DLP can't fetch videos
+### '--no-playlist', if there is a video and a playlist, then only video is going to be selected
+### '--list-formats', list all formats
+### '-o, --output "%(title)s.%(ext)s"', use template to give the name that you want
+### '-I, --playlist-items "[START]:[STOP][:STEP]"', download specific items
+### '--print <template or keyword>', print the details that you want before downloading or doing something
+
+yt-dlp --console-title --progress --no-warnings --quiet --output "%(title)s.%(ext)s" -f "bv[height<=1080]+ba" --print "%(playlist_index)s. %(title)s" <video_url>
