ca9e1ba77e6ad9d8e107fb8a00f7628d603e4d90
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2025-09-12 03:10:32 +0530

    Update yt-dlp.sh

--- Shell Script/yt-dlp.sh
+++ Shell Script/yt-dlp.sh
@@ -4,5 +4,7 @@
 ### '-o, --output "%(title)s.%(ext)s"', use template to give the name that you want
 ### '-I, --playlist-items "[START]:[STOP][:STEP]"', download specific items
 ### '--print <template or keyword>', print the details that you want before downloading or doing something
+### '--skip-download', not going to download any video
 
-yt-dlp --console-title --progress --no-warnings --quiet --output "%(title)s.%(ext)s" -f "bv[height<=1080]+ba" --print "%(playlist_index)s. %(title)s" <video_url>
+yt-dlp --console-title --progress --no-warnings --quiet --output "%(title)s.%(ext)s" -f "bv[height<=1080]+ba" --print "%(playlist_index)s. %(title)s" <url>
+yt-dlp --skip-download --print title <url>
