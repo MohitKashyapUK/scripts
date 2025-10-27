48fbe60d19e5f5bd6f4ca874dec5666711d1f58b
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2025-10-28 01:20:50 +0530

    Update yt-dlp.sh

--- Shell Script/yt-dlp.sh
+++ Shell Script/yt-dlp.sh
@@ -6,6 +6,14 @@
 ### '--print <template or keyword>', print the details that you want before downloading or doing something
 ### '--skip-download', not going to download any video
 
+### Playlist ###
 yt-dlp --output "%(playlist_title)s/%(title)s.%(ext)s" -f "bv[height<=1080]+ba" --cookies-from-browser firefox "https://www.youtube.com/playlist?list=PLYdSUCkqdg67qnz3MEo0NsaM65HjAKsIm"
+
+### Video ###
 yt-dlp --no-playlist --output "%(title)s.%(ext)s" -f "bv[height<=1080]+ba" --cookies-from-browser firefox "https://www.youtube.com/watch?v=eYctE_enPkk&list=PLYdSUCkqdg67qnz3MEo0NsaM65HjAKsIm"
+
+### Video with subtitles ###
+yt-dlp --no-playlist --write-auto-subs --sub-langs en --embed-subs --output "%(title)s.%(ext)s" -f "bv[height<=1080]+ba" --cookies-from-browser firefox <Video_URL>
+
+### Print title ###
 yt-dlp --skip-download --print title "https://www.youtube.com/watch?v=xXHc_lH8-yU"
