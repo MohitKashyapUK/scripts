261a5caa20a1c310ce253085d7fdbcde158e0118
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2025-11-20 11:05:42 +0530

    Update get-roulette-results.js

--- Gambling/get-roulette-results.js
+++ Gambling/get-roulette-results.js
@@ -3,9 +3,11 @@ let bottom_results = document.querySelector(bottom_results_selector).innerText;
 
 console.log(bottom_results.replace(/\n/g, ' '));
 
-bottom_results.split('\n').map(value => {
+let results_string = bottom_results.split('\n').map(value => {
   if (value >= 1 && value <= 12) return 1
   else if (value >= 13 && value <= 24) return 2
   else if (value >= 25 && value <= 36) return 3
   else return 0
-});
+}).reverse().toString().replaceAll(",", "");
+
+console.log(results_string);
