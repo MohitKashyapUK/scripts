4599d29033e591854c0b7a6daa753b951d9afda2
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2026-02-02 21:08:45 +0530

    Update get-dozen-results.js

--- Gambling/get-dozen-results.js
+++ Gambling/get-dozen-results.js
@@ -7,10 +7,10 @@ let results = "";
 for (let x = 0; x < results_array.length; x++) {
   let num = parseInt(results_array[x]); // String ko number mein convert karna safe rehta hai
 
-  /* if (num === 0 || isNaN(num)) {
-    continue; // Zero ko skip kar rahe hain jaisa aapne chaha tha
-  } */
-  if (num >= 1 && num <= 12) {
+  if (num === 0 || isNaN(num)) {
+    results += "0"; // continue; // Zero ko skip kar rahe hain jaisa aapne chaha tha
+  }
+  else if (num >= 1 && num <= 12) {
     results += "1"; // First Dozen
   } 
   else if (num >= 13 && num <= 24) {
